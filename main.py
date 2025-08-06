75ff3f0c8aaf3fe857496acb8d2f08a80e03114b
Jana <janabenn@gmx.de>
2025-08-06 16:43:23 -0400

    SM komplett+1.Spearman SM

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
-from data_loader import load_and_clean_data 
-from AD_interaction import(barplots_vs_ads, plot_ads_engagement_distribution, 
-                           barplots_intent_vs_targets, barplots_discount_vs_targets, 
-                           heatmap_return_by_category_and_ads, heatmap_ads_by_age_and_category,
-                           heatmap_purchase_intent_by_age_and_category, plot_return_share_by_age_group,
-                           plot_ads_users_count_by_age_group, plot_purchase_frequency_by_age, 
-                           plot_purchase_amount_by_age, plot_total_revenue_by_age_group, 
-                           heatmap_return_rate_by_location_and_category)
+from data_loader import load_and_clean_data
 from visualisation import plot_interaction_overview
-
+from SM_interaction import (analyze_and_plot_return_sm_correlation,
+                            correlation_sm_return_overall, correlation_sm_return_furniture)
+from SM_analyse import correlation_matrix_sm_return
 
 def main():
     
-    df = load_and_clean_data()
-    barplots_vs_ads(df)
-    plot_interaction_overview(df)
-    plot_ads_engagement_distribution(df)
-    barplots_intent_vs_targets(df)
-    barplots_discount_vs_targets(df)
-    heatmap_return_by_category_and_ads(df)
-    heatmap_ads_by_age_and_category(df)
-    heatmap_purchase_intent_by_age_and_category(df)
-    plot_return_share_by_age_group(df)
-    plot_ads_users_count_by_age_group(df)
-    plot_purchase_frequency_by_age(df)
-    plot_purchase_amount_by_age(df)
-    plot_total_revenue_by_age_group(df)
-    heatmap_return_rate_by_location_and_category(df)
+    df = load_and_clean_data() 
+    plot_interaction_overview(df) 
+    analyze_and_plot_return_sm_correlation(df)
+    correlation_sm_return_overall(df)
+    correlation_sm_return_furniture(df)
+    correlation_matrix_sm_return(df)
+ 
+    
 
 
 
