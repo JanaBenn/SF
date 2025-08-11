f1299d8709e87d7dd21ec48114e9ef40c9d52874
Jana <janabenn@gmx.de>
2025-08-11 18:35:40 -0400

    Analyse Return

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from data_loader import load_and_clean_data
-from visualisation import plot_interaction_overview
-from AD_and_SM_analyse import run_agegroup_logit_regressions
+from visualisation import plot_interaction_overview, plot_return_logit_summary
+from Return import collect_odds_ratios
+
 
 def main():
     
     df = load_and_clean_data() 
-    plot_interaction_overview(df) 
-    run_agegroup_logit_regressions(df)
+    plot_interaction_overview(df)
+    or_df = collect_odds_ratios(df, alpha=0.8)
+    plot_return_logit_summary(or_df)
+    
   
  
     
