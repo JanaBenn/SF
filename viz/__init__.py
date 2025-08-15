2ad8626e36bc8c937eed036087dc4c09ab0273d4
Jana <janabenn@gmx.de>
2025-08-15 02:30:47 -0400

    init for plots OOP return

--- viz/__init__.py
+++ viz/__init__.py
@@ -2,7 +2,9 @@
 from .plots import (
     plot_social_ads_fourbars, 
     plot_return_logit_summary, 
-    plot_segment_heatmap
+    plot_segment_heatmap,
+    plot_top_returns_table,
+    plot_recommendations_topk_all_ages_table
     
 )
 
@@ -10,4 +12,6 @@ __all__ = [
     "plot_social_ads_fourbars",
     "plot_return_logit_summary",
     "plot_segment_heatmap",
+    "plot_top_returns_table",
+    "plot_recommendations_topk_all_ages_table"
 ]
